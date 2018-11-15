b27365e29850e891ffd75a33ae43f03f7a6e2f0f
rene <renegmed@yahoo.com>
2018-11-15 09:29:19 -0500

    the equivalent test in javascript.

---
+++ test/FundingTest.js
@@ -0,0 +1,12 @@
+// test/FundingTest.js
+const Funding = artifacts.require("Funding");
+
+contract("Funding", accounts => {
+  const [firstAccount] = accounts;
+  
+  it("sets an owner", async () => {
+    const funding = await Funding.new();
+    assert.equal(await funding.owner.call(), firstAccount);
+  });	
+});
+
